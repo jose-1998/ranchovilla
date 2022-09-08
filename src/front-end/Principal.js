dcbd5f87bc70072ac3a51db22878978a667e8df1
jose-1998 <josemrojasortega@gmail.com>
2022-09-08 15:31:49 -0400

    Primer commit

---
+++ src/front-end/Principal.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Principal.css";
+//import Imagen from './front-end/Imagen.js';
+//import img from "../../public/img/principal.jpeg"
+const Principal = () => {
+  return (
+    <div className="title">
+      <div className="preuba">
+      <p>Hola Bienvenido a Rancho Villa Party & Place</p>
+      </div>
+      
+    </div>
+    
+  );
+};
+
+export default Principal;
