d99146dac81cd624a8f39156036d607b3c4dd0fe
jose-1998 <josemrojasortega@gmail.com>
2022-09-12 11:34:46 -0400

    mejorando

--- src/fron-end/Informacion.js
+++ src/fron-end/Informacion.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React from "react";
+import "./Informacion.css";
 
 const Informacion = () => {
-    return (
-        <div>
-            <h2>Servicios :</h2>
-            Prestanos el servio del alquiler del local 
-            akjsdhakjsdhaskdjghasdjkahsdjkh
-            jhagsdjahsgdjaghsdjaghsdjsjagdjahgdjhagsdjaghsd
-            adkjagdasjahgdjaghsdjhagda
-            asdjhasdkjaghkjagfkjgafaf
-            asdfjhagsdjhgfjkadgf
-            adfashgasdfjagfkjgasdf
-        </div>
-    );
-}
+  return (
+    <div id="texto">
+      <h2 className="prueba">Servicios :</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+        voluptate accusantium repellendus maxime non. Quo aut nemo nihil aliquid
+        autem? Quos magnam fugiat modi nostrum commodi sit molestiae ipsum
+        exercitationem! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Numquam, asperiores totam voluptate obcaecati autem doloribus voluptatem
+        vel excepturi quibusdam deserunt eos iusto dolore placeat. Dicta
+        corrupti et eveniet asperiores harum! Optio perferendis eum at molestias
+        iure, commodi, suscipit dolorem eius quae deserunt odio ab. Cumque quis
+        sunt fuga vero quisquam laborum temporibus, quas architecto dolores ab
+        odit, dolore quasi adipisci. Hic, nemo laboriosam veritatis ducimus
+        doloribus mollitia quia saepe maxime atque sequi alias corrupti eum
+        ipsam distinctio porro aliquam amet quis minus. Harum, maxime! Eligendi
+        tempora voluptatibus enim facere rem. Nemo, numquam. Veritatis quis
+        atque suscipit dolorem nostrum voluptatum ducimus expedita id iste,
+        sapiente ea totam hic sint animi rem iusto repellendus quod possimus?
+        Similique sint ratione earum ut doloremque? Velit ratione, unde rem
+        animi deleniti officia doloribus iste quam, officiis quod sunt vitae
+        harum a eos sit cumque error perspiciatis cum qui iure explicabo
+        quisquam accusamus! Magnam, suscipit atque. Nisi fugit id, vitae saepe,
+        repellat inventore vero, voluptatum placeat impedit molestiae
+        temporibus. Inventore ex accusantium accusamus eveniet expedita est
+        natus numquam, assumenda delectus reprehenderit ipsum
+      </p>
+    </div>
+  );
+};
 
 export default Informacion;
